8f91e23dbac7c035bb564b15eb5f75a5c575fa4d
menoo-o <menosuper6@gmail.com>
2025-04-07 15:55:23 +0500

    oAuth2.0 bug

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -5,7 +5,7 @@ import { createClient } from '@/utils/supabase/server'
 export async function GET(request: Request) {
   const { searchParams, origin } = new URL(request.url)
   const code = searchParams.get('code')
-  // if "next" is in param, use it as the redirect URL to private dashboard
+  // if "next" is in param, use it as the redirect URL
   const next = searchParams.get('next') ?? '/private'
 
   if (code) {
@@ -13,12 +13,16 @@ export async function GET(request: Request) {
     const { error } = await supabase.auth.exchangeCodeForSession(code)
     if (!error) {
       const forwardedHost = request.headers.get('x-forwarded-host') // original origin before load balancer
+      
       const isLocalEnv = process.env.NODE_ENV === 'development'
+      ? 'http://localhost:3000/auth/callback'
+      : 'https://template-setup.vercel.app/auth/callback';
+
       if (isLocalEnv) {
         // we can be sure that there is no load balancer in between, so no need to watch for X-Forwarded-Host
         return NextResponse.redirect(`${origin}${next}`)
       } else if (forwardedHost) {
-        return NextResponse.redirect(`https://${forwardedHost}${next}`)
+        return NextResponse.redirect(`https://template-setup.vercel.app/${forwardedHost}${next}`)
       } else {
         return NextResponse.redirect(`${origin}${next}`)
       }
