4e7b1b12c3bed960cc7658b68871aa872c56269b
menoo-o <menosuper6@gmail.com>
2025-04-09 18:33:52 +0500

    reset passwd pg debug pt2

--- app/account/reset-password/actions.ts
+++ app/account/reset-password/actions.ts
@@ -5,12 +5,12 @@ export async function sendResetEmail(email: string) {
   const supabase = await createClient();
 
 //  Send reset password email
-  const { error } = await supabase.auth.resetPasswordForEmail(email, {
-    redirectTo: process.env.NODE_ENV === 'development'
-    ? 'http://localhost:3000/auth/confirm'
-    : 'https://template-setup.vercel.app/auth/confirm',
+const { error } = await supabase.auth.resetPasswordForEmail(email, {
+  redirectTo: process.env.NODE_ENV === 'development'
+  ? 'http://localhost:3000/account/update-password'
+  : 'https://template-setup.vercel.app/account/update-password',
 
-  });
+});
 
   return { error: error?.message || null };
 }
