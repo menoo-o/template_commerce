cae7dae217a888d661bbe0417d5b51892efb5a5d
menoo-o <menosuper6@gmail.com>
2025-04-28 10:14:59 +0500

    vercel debug

--- app/checkout/page.tsx
+++ app/checkout/page.tsx
@@ -1,1 +1,12 @@
 // Incomplete Checkout Process
+import React from 'react'
+
+function CheckOutpage() {
+  return (
+    <div>
+      this is the checkout page
+    </div>
+  )
+}
+
+export default CheckOutpage
