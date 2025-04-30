d562e0d8d7c278a2c75fe8569b5fc103e2cd3da9
menoo-o <menosuper6@gmail.com>
2025-04-30 12:13:45 +0500

    single pg isnt finished yet

--- app/collections/[collectionSlug]/products/[productSlug]/page.tsx
+++ app/collections/[collectionSlug]/products/[productSlug]/page.tsx
@@ -1,1 +1,13 @@
 // [collectionSlug]/products/[productSlug]/page.tsx — displays individual product detail.
+
+import React from 'react'
+
+function Singleproductpage() {
+  return (
+    <div>
+      <h2>Single Product Page</h2>
+    </div>
+  )
+}
+
+export default Singleproductpage
