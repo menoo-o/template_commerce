f09827481bc7ebeabcf24d1d0977a496a57d8924
menoo-o <menosuper6@gmail.com>
2025-05-14 12:54:25 +0500

    single page complete pt1.3

--- app/collections/[collectionSlug]/products/[productSlug]/loading.tsx
+++ app/collections/[collectionSlug]/products/[productSlug]/loading.tsx
@@ -6,7 +6,7 @@ export default function Loading() {
     <div className="min-h-screen bg-white flex items-center justify-center">
       <div className="flex flex-col items-center space-y-4">
         <Loader2 className="h-8 w-8 animate-spin text-orange-500" />
-        <p className="text-lg text-gray-500">Loading products...</p>
+        
       </div>
     </div>
   );
