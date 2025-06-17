a8d9c8a7d4d15509352d9403ba27c3f1fbbe2d7a
menoo-o <menosuper6@gmail.com>
2025-06-17 13:46:49 +0500

    checkour page pt1.3

--- app/success/page.tsx
+++ app/success/page.tsx
@@ -2,18 +2,22 @@
 
 import { useSearchParams } from 'next/navigation'
 import { Suspense, useEffect, useState } from 'react'
+import { useCartStore } from '@/stores/useCartStore';
 
 function SuccessContent() {
   const searchParams = useSearchParams()
   const [paymentIntentId, setPaymentIntentId] = useState<string | null>(null)
+  const { clearCart}  = useCartStore();
 
-  useEffect(() => {
-    const id = searchParams.get('paymentIntentId')
-    if (id) {
-      setPaymentIntentId(id)
-      console.log('Success Page Payment Intent ID:', id)
-    }
-  }, [searchParams])
+useEffect(() => {
+  const id = searchParams.get('paymentIntentId')
+  if (id) {
+    setPaymentIntentId(id)
+    clearCart(); // Now safe to do it
+    console.log('Success Page Payment Intent ID:', id)
+  }
+}, [searchParams, clearCart])
+
 
   return (
     <div className="p-4">
