776d9c4bf936e1208ea233fd2fc6d886a1d9bac4
menoo-o <menosuper6@gmail.com>
2025-06-23 11:50:09 +0500

    api route#1 created, mini nav changes

--- components/Mininavbar/Navbar.tsx
+++ components/Mininavbar/Navbar.tsx
@@ -5,13 +5,12 @@ function Navbar() {
   return (
     <div>
         <nav className="bg-orange-500 text-white py-4 px-8 flex justify-between items-center shadow-md">
-      <div className="text-2xl font-bold">MyShop</div>
+      <Link href="/" className="text-2xl font-bold">MyShop</Link>
       <div className="flex space-x-6">
         <Link href="/" className="hover:underline">Home</Link>
         <Link href="/collections/all-products" className="hover:underline">Shop</Link>
         <Link href="/account/login" className="hover:underline">Login</Link>
         <Link href="/cart" className="hover:underline">🛒 Cart</Link>
-        <Link href="/about" className="hover:underline">About</Link>
       </div>
     </nav>
     </div>
